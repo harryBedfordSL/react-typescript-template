c776b6c9a50db8472990edd463c7504d8570aeab
Harry Bedford <hbedford@scottlogic.com>
2021-12-23 15:52:21 +0000

    add eslint to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './app.module.css';
 
-export const App = () => {
+export const App = (): JSX.Element => {
   return (
     <div className={styles.container}>
       <div className={styles.appHeading}>WELCOME</div>
     </div>
-  )
-}
+  );
+};
