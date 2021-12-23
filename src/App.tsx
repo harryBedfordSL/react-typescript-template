ad1c84b2f270fddd3a65fe427e388e91d68db7dc
Harry Bedford <hbedford@scottlogic.com>
2021-12-23 15:12:29 +0000

    add font to project

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import styles from './app.module.css';
 
 export const App = () => {
-  return <div className={styles.appHeading}>Hello from React World</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.appHeading}>WELCOME</div>
+    </div>
+  )
 }
